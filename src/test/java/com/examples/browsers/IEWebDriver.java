e9844f9030b2172cff6b3f97c2fb61d41ff1e7ca
chathurabr <chathura.br@gmail.com>
2017-04-19 12:38:48 +0530

    new

---
+++ src/test/java/com/examples/browsers/IEWebDriver.java
@@ -0,0 +1,24 @@
+package com.examples.browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Chathura on 12/30/2016.
+ */
+public class IEWebDriver {
+    WebDriver driver;
+
+    @Test
+    public void base () {
+
+        System.setProperty("webdriver.ie.driver", "C:\\IEDriverServer.exe");
+        driver = new InternetExplorerDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.get("http://opensource.demo.orangehrmlive.com/");
+    }
+}
---
+++ src/test/java/com/examples/browsers/OperaWebDriver.java
@@ -0,0 +1,23 @@
+package com.examples.browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Chathura on 12/30/2016.
+ */
+public class OperaWebDriver {
+    WebDriver driver;
+
+    @Test
+    public void base () {
+        System.setProperty("webdriver.opera.driver", "C:\\operadriver.exe");
+        driver = new OperaDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.get("http://opensource.demo.orangehrmlive.com/");
+    }
+}
